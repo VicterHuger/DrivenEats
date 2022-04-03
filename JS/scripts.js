4ca56ffb25d31578da1a2828ee0307a1fa25f316
Victer_Huger <victorhugobs@eq.ufrj.br>
2022-04-03 00:07:28 -0300

    Correção de sombra da seleção de itens; Correção do texto do botão de fechar pedido; Botão de finalizar pedido em funcionamento

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,9 @@
-// let prato;
-// let bebida;
-// let sobremesa;
+//Variáveis globais
+let prato;
+ let bebida;
+ let sobremesa;
 
+ //função que verifica e seleciona um prato
 function selecionarPrato(elemento){
     let pratoSelecionado = document.querySelector(".selecionado-prato");
     let iconeSelecionado = document.querySelector(".icone-clicado-prato");
@@ -11,8 +13,9 @@ function selecionarPrato(elemento){
     }
     elemento.classList.add("selecionado-prato");
     elemento.querySelector("ion-icon").classList.add("icone-clicado-prato");
+    prato=elemento;
 }
-
+ //função que verifica e seleciona uma bebida
 function selecionarBebiba(elemento){
     let bebidaSelecionada = document.querySelector(".selecionado-bebida");
     let iconeSelecionado = document.querySelector(".icone-clicado-bebida");
@@ -22,8 +25,9 @@ function selecionarBebiba(elemento){
     }
     elemento.classList.add("selecionado-bebida");
     elemento.querySelector("ion-icon").classList.add("icone-clicado-bebida");
+    bebida=elemento;
 }
-
+ //função que verifica e seleciona uma sobremesa
 function selecionarSobremesa(elemento){
     let sobremesaSelecionada = document.querySelector(".selecionado-sobremesa");
     let iconeSelecionado = document.querySelector(".icone-clicado-sobremesa");
@@ -33,5 +37,13 @@ function selecionarSobremesa(elemento){
     }
     elemento.classList.add("selecionado-sobremesa");
     elemento.querySelector("ion-icon").classList.add("icone-clicado-sobremesa");
+    sobremesa=elemento;
 }
-
+//função que habilita o botão de finalização do pedido
+function habilitarBotao(){
+    if(prato && bebida  && sobremesa){
+        document.querySelector(".botao-fechar-pedido").classList.add("habilitar-botao-pedido");
+        document.querySelector(".selecionar-pedidos").classList.add("nao-habilitado");
+        document.querySelector(".texto-pedido").classList.add("habilitado");
+    }
+}
