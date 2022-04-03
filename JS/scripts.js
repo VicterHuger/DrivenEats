3466c765baca476fd6e9fe16b7b6802323ebdedd
Victer_Huger <victorhugobs@eq.ufrj.br>
2022-04-03 18:08:32 -0300

    Versão final do projeto; Adicionado o envio das informações do pedido pelo whatsapp ao clicar no botão de finalizar o pedido

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,7 @@
-//Variáveis globais
+//Variáveis globais - elementos das opções selecionadas
 let prato;
- let bebida;
- let sobremesa;
+let bebida;
+let sobremesa;
 
  //função que verifica e seleciona um prato
 function selecionarPrato(elemento){
@@ -14,18 +14,20 @@ function selecionarPrato(elemento){
     elemento.classList.add("selecionado-prato");
     elemento.querySelector("ion-icon").classList.add("icone-clicado-prato");
     prato=elemento;
+    habilitarBotao();
 }
  //função que verifica e seleciona uma bebida
 function selecionarBebiba(elemento){
     let bebidaSelecionada = document.querySelector(".selecionado-bebida");
     let iconeSelecionado = document.querySelector(".icone-clicado-bebida");
     if(bebidaSelecionada !== null && iconeSelecionado !== null){
-        bebidaSelecionada.classList.remove("selecionado-bebida")
+        bebidaSelecionada.classList.remove("selecionado-bebida");
         iconeSelecionado.classList.remove("icone-clicado-bebida");
     }
     elemento.classList.add("selecionado-bebida");
     elemento.querySelector("ion-icon").classList.add("icone-clicado-bebida");
     bebida=elemento;
+    habilitarBotao();
 }
  //função que verifica e seleciona uma sobremesa
 function selecionarSobremesa(elemento){
@@ -38,6 +40,7 @@ function selecionarSobremesa(elemento){
     elemento.classList.add("selecionado-sobremesa");
     elemento.querySelector("ion-icon").classList.add("icone-clicado-sobremesa");
     sobremesa=elemento;
+    habilitarBotao();
 }
 //função que habilita o botão de finalização do pedido
 function habilitarBotao(){
@@ -45,5 +48,31 @@ function habilitarBotao(){
         document.querySelector(".botao-fechar-pedido").classList.add("habilitar-botao-pedido");
         document.querySelector(".selecionar-pedidos").classList.add("nao-habilitado");
         document.querySelector(".texto-pedido").classList.add("habilitado");
+        linkWhatsapp();
     }
+}
+//função que devolve o valor do item selecionado dentro do padrão esperado para o texto no whatsapp  
+function valorItem(elemento){
+   let valorstring=elemento.querySelector(".preco").innerHTML;
+   let valornumerico=Number(converterMoeda(valorstring));
+   return valornumerico;
+}
+//função que converte o valor em string da opção escolhida em uma string que possa ser convertida em valor numerico
+function converterMoeda(string){
+    return string.replace('R$ ','').replace(',','.');
+} 
+//função que encodifiaca a mensagem do whatssapp e modifica o elemento clicado com o link
+function linkWhatsapp(){
+    let valorprato=valorItem(prato);
+    let valorbebida=valorItem(bebida);
+    let valorsobremesa=valorItem(sobremesa);
+    let valortotal=valorprato+valorbebida+valorsobremesa;
+    valortotal=valortotal.toFixed(2);
+    valortotal=`R$ ${valortotal}`;
+    let nomeprato=prato.querySelector(".titulo-do-item").innerHTML;
+    let nomebebida=bebida.querySelector(".titulo-do-item").innerHTML;
+    let nomesobremesa=sobremesa.querySelector(".titulo-do-item").innerHTML;
+    let texto=`Olá, gostaria de fazer o pedido:\n- Prato: ${nomeprato}\n- Bebida: ${nomebebida}\n- Sobremesa: ${nomesobremesa}\nTotal: ${valortotal}`;
+    let link=`https://wa.me/5521968045123?text=${encodeURIComponent(texto)}`;
+    document.querySelector(".link-whatsapp").setAttribute("href", link );
 }
