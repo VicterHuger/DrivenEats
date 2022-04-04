4ec8f0136feab2e8efc2270a717a36697a46152c
Victer_Huger <victorhugobs@eq.ufrj.br>
2022-04-04 01:34:39 -0300

    Versão final com bônus - Adicionado tela de confiramação do pedido e envio com as informações de nome e endereço pelo whatsapp

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,19 @@
-//Variáveis globais - elementos das opções selecionadas
+//Variáveis globais  
+//elementos das opções selecionadas
 let prato;
 let bebida;
 let sobremesa;
-
+//nome e preço dos itens selecionados
+let precoprato;
+let precobebida;
+let precosobremesa;
+let valortotal;
+let nomeprato;
+let nomebebida;
+let nomesobremesa;
+//nome e endereco
+let nome;
+let endereco;
  //função que verifica e seleciona um prato
 function selecionarPrato(elemento){
     let pratoSelecionado = document.querySelector(".selecionado-prato");
@@ -48,7 +59,7 @@ function habilitarBotao(){
         document.querySelector(".botao-fechar-pedido").classList.add("habilitar-botao-pedido");
         document.querySelector(".selecionar-pedidos").classList.add("nao-habilitado");
         document.querySelector(".texto-pedido").classList.add("habilitado");
-        linkWhatsapp();
+        //linkWhatsapp();
     }
 }
 //função que devolve o valor do item selecionado dentro do padrão esperado para o texto no whatsapp  
@@ -63,16 +74,41 @@ function converterMoeda(string){
 } 
 //função que encodifiaca a mensagem do whatssapp e modifica o elemento clicado com o link
 function linkWhatsapp(){
-    let valorprato=valorItem(prato);
-    let valorbebida=valorItem(bebida);
-    let valorsobremesa=valorItem(sobremesa);
-    let valortotal=valorprato+valorbebida+valorsobremesa;
-    valortotal=valortotal.toFixed(2);
-    valortotal=`R$ ${valortotal}`;
-    let nomeprato=prato.querySelector(".titulo-do-item").innerHTML;
-    let nomebebida=bebida.querySelector(".titulo-do-item").innerHTML;
-    let nomesobremesa=sobremesa.querySelector(".titulo-do-item").innerHTML;
-    let texto=`Olá, gostaria de fazer o pedido:\n- Prato: ${nomeprato}\n- Bebida: ${nomebebida}\n- Sobremesa: ${nomesobremesa}\nTotal: ${valortotal}`;
+    let texto=`Olá, gostaria de fazer o pedido:\n- Prato: ${nomeprato}\n- Bebida: ${nomebebida}\n- Sobremesa: ${nomesobremesa}\nTotal: ${valortotal}\n\nNome: ${nome}\nEndereço: ${endereco}`;
     let link=`https://wa.me/5521968045123?text=${encodeURIComponent(texto)}`;
     document.querySelector(".link-whatsapp").setAttribute("href", link );
+}
+//Bonus-função que pede endereço e gera a tela de confirmacao
+function enderecoConfirmacao(){
+    if(document.querySelector(".botao-fechar-pedido").classList.contains("habilitar-botao-pedido")){
+        nome=prompt(`Olá! Já iremos processar seu pedido! Primeiro, nos diga, qual seu nome?`);
+        endereco=prompt(`Certo, ${nome}! Estamos quase lá! Nos diga agora, qual o endereço de entrega do pedido? Escreva no seguinte formato: Rua xxxxxx, nº, complemento, CEP`); 
+        if(nome && endereco){
+            document.querySelector(".tela-de-confirmacao").classList.remove("nao-habilitado");
+            let valorprato=valorItem(prato);
+            let valorbebida=valorItem(bebida);
+            let valorsobremesa=valorItem(sobremesa);
+            valortotal=valorprato+valorbebida+valorsobremesa;
+            valortotal=valortotal.toFixed(2);
+            valortotal=`R$ ${valortotal}`;
+            valortotalvirgula=valortotal.replace('.',',');
+            precoprato=prato.querySelector(".preco").innerHTML;
+            precobebida=bebida.querySelector(".preco").innerHTML;
+            precosobremesa=sobremesa.querySelector(".preco").innerHTML;
+            nomeprato=prato.querySelector(".titulo-do-item").innerHTML;
+            nomebebida=bebida.querySelector(".titulo-do-item").innerHTML;
+            nomesobremesa=sobremesa.querySelector(".titulo-do-item").innerHTML;
+            document.querySelector(".item-prato").innerHTML=nomeprato;
+            document.querySelector(".preco-prato").innerHTML=precoprato;
+            document.querySelector(".item-bebida").innerHTML=nomebebida;
+            document.querySelector(".preco-bebida").innerHTML=precobebida;
+            document.querySelector(".item-sobremesa").innerHTML=nomesobremesa;
+            document.querySelector(".preco-sobremesa").innerHTML=precosobremesa;
+            document.querySelector(".preco-total").innerHTML=valortotalvirgula;
+            linkWhatsapp();
+        }
+    }   
+}
+function cancelarPedido(){
+    document.querySelector(".tela-de-confirmacao").classList.add("nao-habilitado")
 }
